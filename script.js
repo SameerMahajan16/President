8acd0469399571a1ba6c98fd1cd11b8066915e7c
SameerMahajan16 <samneomah@gmail.com>
2025-04-09 04:22:00 +0000

    done

---
+++ script.js
@@ -0,0 +1,14 @@
+function showFact() {
+    const fact = "Jimmy Carter was the first U.S. president born in a hospital!";
+    document.getElementById("funFact").innerText = fact;
+}
+
+function showQuote() {
+    const quote = "We must adjust to changing times and still hold to unchanging principles. - Jimmy Carter";
+    document.getElementById("quote").innerText = quote;
+}
+
+function showWork() {
+    const work = "Carter has helped build over 4,000 homes with Habitat for Humanity.";
+    document.getElementById("work").innerText = work;
+}
